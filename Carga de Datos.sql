c01326d93a63047d5b71955e88caaa7ac1dd2301
notcharlie28 <carlos.obando.avendana@est.una.ac.cr>
2019-06-06 13:49:52 -0600

    ordenamiento
    ordemamiento de las carpetas

--- Carga de Datos.sql
+++
@@ -1,4 +0,0 @@
-use bd_votaciones;
-
-INSERT INTO `bd_votaciones`.`usuario` (`cedula`, `apellido1`, `apellido2`, `nombre`, `clave`, `activo`) VALUES ('111', 'ggg', 'hhh', 'pete', '111', '0');
-INSERT INTO `bd_votaciones`.`administrador` (`cedula`, `apellido1`, `apellido2`, `nombre`, `usuario`, `clave`) VALUES ('222', 'kkk', 'lll', 'hjk', 'qwe', '123');
